e53db8fd542c2575e9577f3868703efeef2b034a
IvanMak626 <341034262@gapps.yrdsb.ca>
2023-11-10 08:35:20 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,14 +2,17 @@ public class Student()
 {
   private String firstName;
   private String lastName;
+//  modifications
+  private int grade;
 
-  public Student(String fName, String lName)
+  public Student(String fName, String lName, int theGrade)
   {
     firstName = fName;
     lastName = lName;
+    grade = theGrade;
   }
 
   public String toString()
   {
-    return "The name of this student is " + firstName + " " + lastName;
+    return "The name of this student is " + firstName + " " + lastName+ "with a grade of " + grade;
 }
